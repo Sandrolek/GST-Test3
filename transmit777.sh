350c1db67f6ac6ab1af76fb1c21dd7b92db2fbd8
Alexander <sandrolek06@gmail.com>
2018-03-13 22:15:16 +0300

    Добавил файл Test_Joy_XMLRPC.py

--- transmit777.sh
+++ transmit777.sh
@@ -1,2 +1,2 @@
-gst-launch-1.0 v4l2src device="/dev/video0" ! image/jpeg,width=640,height=480,framerate=30/1 ! \
+gst-launch-1.0 v4l2src device="/dev/video1" ! image/jpeg,width=800,height=600,framerate=30/1 ! \
 jpegdec ! autovideosink sync=false
--- transmit_raw.sh
+++ transmit_raw.sh
@@ -1,3 +1,3 @@
-gst-launch-1.0 v4l2src device='/dev/video0' ! jpegenc ! \
-rtpjpegpay ! autovideosink ! udpsink host=127.0.0.1 port=9000
+gst-launch-1.0 v4l2src device='/dev/video0' ! video/x-raw,width=640,height=480,framerate=30/1 ! \
+autovideosink sync=false
 
